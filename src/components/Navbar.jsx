929ab1e43fadcf1583a18fba510ecaaee0ede9f1
sameerdewan <sameer.dewan@itential.com>
2021-03-14 01:29:45 -0500

    semi responsive?

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Menubar } from 'primereact/menubar';
 import { Button } from 'primereact/button';
 import logo from '../images/poetry.svg';
 
-const ShamanNavbar = styled(Menubar)`
+const PoetryNavbar = styled(Menubar)`
     background: white;
     font-family: poetry;
     & .p-menuitem-text {
         color: var(--poetry_brand) !important;
-        font-size: 24px;
+        font-size: 2vw;
         margin-left: 5px;
     };
     & .p-menubar-button {
@@ -19,13 +19,16 @@ const ShamanNavbar = styled(Menubar)`
     };
 `;
 
-const ShamanLogo = () => {
+const SafeLogo = styled.img`
+    height: 3vw;
+`;
+
+const PoetryLogo = () => {
     return (
         // eslint-disable-next-line jsx-a11y/anchor-is-valid
         <a href="#" role="menuitem" class="p-menuitem-link" aria-haspopup="false">
-            <img 
-                src={logo} 
-                height='40'
+            <SafeLogo 
+                src={logo}
                 alt='poetry'
             />
             <span class="p-menuitem-text">poetry</span>
@@ -46,8 +49,8 @@ const Menu = () => {
 
 function Navbar() {
     return (
-        <ShamanNavbar
-            start={<ShamanLogo />}
+        <PoetryNavbar
+            start={<PoetryLogo />}
             end={<Menu />}
         />
     );
--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,8 +1,12 @@
 import styled from '@emotion/styled'
+import { Button } from 'primereact/button';
 import HomepageImage from '../images/homepage.svg';
 
 const TopMarginGrid15px = styled.div`
     margin-top: 15px;
+    & .margin-right-10px {
+        margin-right: 10px !important;
+    };
 `;
 
 const FlipImage = styled.img`
@@ -10,18 +14,56 @@ const FlipImage = styled.img`
     transform: scaleX(-1) !important;
 `;
 
-const HeaderText = styled.span`
+const HomePageHeaderGrid = styled.div`
+    margin-top: 40px;
+    padding: 30px;
+`;
+
+const HeaderText = styled.section`
     font-family: poetry;
     color: var(--poetry_brand);
+    font-size: 1.7vw;
+    font-weight: bolder;
+`;
+
+const SubHeaderText = styled.section`
+    font-family: poetry;
+    color: var(--poetry_brand);
+    font-size: 1.5vw;
+    font-weight: 100;
+`;
+
+const ButtonsContainer = styled.section`
+    margin-top: 25px;
+    width: 100%;
+    display: inline-flex;
+    justify-content: left;
+    padding-left: 30px;
 `;
 
 function Homepage() {
     return (
         <TopMarginGrid15px className='p-grid'>
             <div className='p-col-6'>
-                <HeaderText>
-                    the speed of light
-                </HeaderText>
+                <HomePageHeaderGrid>
+                    <HeaderText>
+                        Generate immutable, decentralized proofs for your files that are cross-blockchain resilient
+                    </HeaderText>
+                    <SubHeaderText>
+                        <ul>
+                            <li> No tokens required</li>
+                            <li>Persist proofs to multiple chains (Ethereum, Harmony, Polkadot)</li>
+                            <li>Create proofs for <i>numerous</i> file types</li>
+                            <li>Easy to use interface</li>
+                            <li>Seamless plug and play API integration</li>
+                            <li>Solutions available for all - from casual to enterprise</li>
+                        </ul>
+                    </SubHeaderText>
+                </HomePageHeaderGrid>
+                <ButtonsContainer>
+                    <Button label='Get started' className='margin-right-10px'/>
+                    <Button label='Documentation' className='p-button-outlined margin-right-10px' />
+                </ButtonsContainer>
             </div>
             <div className='p-col-6'>
                 <FlipImage src={HomepageImage} />
