76b27021be78fc8b6b72545a84a3ce255e8b806d
sameerdewan <sameer.dewan@itential.com>
2021-03-14 12:05:00 -0400

    complete home page bottom footer

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 import { Button } from 'primereact/button';
 import React from 'react';
+import logoWhite from '../images/poetry-white.svg';
 import HomepageImage from '../images/homepage.svg';
 
 const TopMarginGrid15px = styled.div`
@@ -42,10 +43,25 @@ const ButtonsContainer = styled.section`
     padding-left: 30px;
 `;
 
+const SafeImage = styled.img`
+    height: 2.25vw;
+`;
+
 const BottomContainer = styled.section`
     background: var(--poetry_brand);
-    height: 400px;
     width: 101vw;
+    padding: 25px 150px;
+    position: relative;
+    bottom: 0;
+    & .poetry-footer-logo {
+        color: white;
+        font-size: 1.5vw;
+    };
+    & .white-text {
+        color: white;
+        font-family: effra,helvetica,arial,sans-serif;
+        font-size: 1vw;
+    };
 `;
 
 function Homepage() {
@@ -78,7 +94,36 @@ function Homepage() {
                 </div>
             </TopMarginGrid15px>
             <BottomContainer className="p-grid">
-
+                <div className='p-col-2'>
+                    <section className='poetry-footer-logo'>
+                        <SafeImage src={logoWhite} />
+                        poetry
+                    </section>
+                    <p className='white-text'>
+                        poetry Ltd. is registered in the US. 1602 Mockingbird Hill, Dalton, GA
+                    </p>
+                </div>
+                <div className='p-col-1'/>
+                <div className='p-col-2'>
+                    <Button label='Product'/><br/>
+                    <Button className='p-button-sm' label='Basic' /><br/>
+                    <Button className='p-button-sm' label='Professional' /><br/>
+                    <Button className='p-button-sm' label='Small Business' /><br/>
+                    <Button className='p-button-sm' label='Enterprise' /><br/>
+                </div>
+                <div className='p-col-3'>
+                    <Button label='Resources'/><br/>
+                    <Button className='p-button-sm' label='Getting Started' /><br/>
+                    <Button className='p-button-sm' label='API Documentation' /><br/>
+                </div>
+                <div className='p-col-3'>
+                    <Button label='Legal'/><br/>
+                    <Button className='p-button-sm' label='License' /><br/>
+                    <Button className='p-button-sm' label='Privacy Statement' /><br/>
+                </div>
+                <div className='p-col-1'>
+                    <Button label='Social'/>
+                </div>
             </BottomContainer>
         </React.Fragment>
     );
