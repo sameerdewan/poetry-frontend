d89d18e17418dce98703877886af8f1b18473566
sameerdewan <sameer.dewan@itential.com>
2021-03-13 22:12:49 -0500

    poetry navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,24 @@
+import styled from '@emotion/styled';
 import { Menubar } from 'primereact/menubar';
+
+const ShamanNavbar = styled(Menubar)`
+    background: white;
+    box-shadow: rgba(0, 0, 0, 0.0) 0px 6px 24px 0px, rgba(0, 0, 0, 0.9) 0px 0px 0px 1px;
+    font-family: poetry;
+    & .p-menuitem-text {
+        color: black !important;
+    };
+`;
 
 function Navbar() {
     const items = [
         {
-            template: () => {
-                
-            }
+            label: 'poetry',
+            icon: 'pi pi-fw shaman-logo'
         }
       ];
     return (
-        <Menubar 
+        <ShamanNavbar 
             model={items}
         />
     );
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import 'primereact/resources/themes/bootstrap4-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import './styles/main.css';
 
 PrimeReact.ripple = true;
 
