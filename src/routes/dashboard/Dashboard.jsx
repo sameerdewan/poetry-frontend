35a38b0da6a483d86b9a4f632b5a778f0fa02999
sameerdewan <sameer.dewan@itential.com>
2021-03-15 18:27:00 -0400

    accordion tree menu

--- src/routes/dashboard/Dashboard.jsx
+++ src/routes/dashboard/Dashboard.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Sidebar from './components/Sidebar';
+import Layout from './components/Layout';
 
 function Dashboard() {
     return (
         <React.Fragment>
-            <Sidebar />
+            <Layout />
         </React.Fragment>
     );
 }
---
+++ src/routes/dashboard/components/Layout.jsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Button } from 'primereact/button';
+import { Accordion, AccordionTab } from 'primereact/accordion';
+import { Tree } from 'primereact/tree';
+
+const SidePanel = styled.section`
+    height: calc(100vh - 100px);
+    position: relative;
+    width: 250px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;
+    & .p-accordion-tab {
+        box-shadow: none !important;
+        -webkit-box-shadow: none !important;
+    };
+    & .p-accordion-header-link {
+        color: var(--poetry_brand) !important;
+    };
+    & .p-accordion .p-accordion-header .p-accordion-header-link {
+        padding: 1rem !important;
+    };
+    & .p-tree {
+        border: none !important;
+        padding: 0 !important;
+        font-size: 12px !important;
+    };
+    & .p-tree .p-tree-container .p-treenode,
+      .p-accordion .p-accordion-content, 
+      .p-tree .p-tree-container .p-treenode .p-treenode-content {
+        padding: 0 !important;
+    };
+    & .p-tree-toggler-icon {
+        font-size: 12px;
+    };
+`;
+
+const BottomSidePanel = styled.footer`
+    position: absolute;
+    bottom: -50px;
+    height: 50px;
+    width: 250px;
+    background: var(--poetry_brand);
+    box-shadow: rgba(0, 0, 0, 0.15) -7px 3px 3px 6px;
+    display: flex;
+    justify-content: flex-end;
+`;
+
+const TopPanel = styled.header`
+    background: var(--poetry_brand);
+    border-bottom: 1px solid var(--poetry_brand);
+    height: 50px;
+    width: 100vw;
+    font-family: poetry;
+    color: var(--poetry_brand);
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 3px 3px 0px;
+`;
+
+const BottomPanel = styled.footer`
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;
+    color: white;
+    width: calc(100vw - 249px);
+    left: 249px;
+    position: absolute;
+    bottom: 0;
+    height: 50px;
+`;
+
+const Container = styled.div`
+    display: inline-flex;
+`;
+
+const data = [
+    {
+        "key": "0",
+        "label": "Documents",
+        "data": "Documents Folder",
+        "icon": "pi pi-fw pi-inbox",
+        "children": [{
+            "key": "0-0",
+            "label": "Work",
+            "data": "Work Folder",
+            "icon": "pi pi-fw pi-cog",
+            "children": [{ "key": "0-0-0", "label": "Expenses.doc", "icon": "pi pi-fw pi-file", "data": "Expenses Document" }, { "key": "0-0-1", "label": "Resume.doc", "icon": "pi pi-fw pi-file", "data": "Resume Document" }]
+        },
+        {
+            "key": "0-1",
+            "label": "Home",
+            "data": "Home Folder",
+            "icon": "pi pi-fw pi-home",
+            "children": [{ "key": "0-1-0", "label": "Invoices.txt", "icon": "pi pi-fw pi-file", "data": "Invoices for this month" }]
+        }]
+    },
+    {
+        "key": "1",
+        "label": "Events",
+        "data": "Events Folder",
+        "icon": "pi pi-fw pi-calendar",
+        "children": [
+            { "key": "1-0", "label": "Meeting", "icon": "pi pi-fw pi-calendar-plus", "data": "Meeting" },
+            { "key": "1-1", "label": "Product Launch", "icon": "pi pi-fw pi-calendar-plus", "data": "Product Launch" },
+            { "key": "1-2", "label": "Report Review", "icon": "pi pi-fw pi-calendar-plus", "data": "Report Review" }]
+    },
+    {
+        "key": "2",
+        "label": "Movies",
+        "data": "Movies Folder",
+        "icon": "pi pi-fw pi-star",
+        "children": [{
+            "key": "2-0",
+            "icon": "pi pi-fw pi-star",
+            "label": "Al Pacino",
+            "data": "Pacino Movies",
+            "children": [{ "key": "2-0-0", "label": "Scarface", "icon": "pi pi-fw pi-video", "data": "Scarface Movie" }, { "key": "2-0-1", "label": "Serpico", "icon": "pi pi-fw pi-video", "data": "Serpico Movie" }]
+        },
+        {
+            "key": "2-1",
+            "label": "Robert De Niro",
+            "icon": "pi pi-fw pi-star",
+            "data": "De Niro Movies",
+            "children": [{ "key": "2-1-0", "label": "Goodfellas", "icon": "pi pi-fw pi-video", "data": "Goodfellas Movie" }, { "key": "2-1-1", "label": "Untouchables", "icon": "pi pi-fw pi-video", "data": "Untouchables Movie" }]
+        }]
+    }
+]
+
+function Layout() {
+    return (
+        <React.Fragment>
+            <TopPanel>
+
+            </TopPanel>
+            <Container>
+                <SidePanel>
+                    <Accordion>
+                        <AccordionTab header='Files'>
+                            <Tree value={data} />
+                        </AccordionTab>
+                        <AccordionTab header='Settings'>
+
+                        </AccordionTab>
+                    </Accordion>
+                    <BottomSidePanel>
+                        <Button icon='pi pi-angle-double-right' />
+                    </BottomSidePanel>
+                </SidePanel>
+            </Container>
+            <BottomPanel>
+
+            </BottomPanel>
+        </React.Fragment>
+    );
+}
+
+export default Layout;
